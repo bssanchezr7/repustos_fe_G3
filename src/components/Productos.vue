18889e826c0b2d56d770d044a0cec3ba3c72e59a
Brandon S <bssanchezr7@gmail.com>
2021-10-27 02:53:03 -0500

    fucionalidades de agregar, actualizar y eliminar

--- src/components/Productos.vue
+++ src/components/Productos.vue
@@ -4,29 +4,55 @@
         <h1>Tus productos</h1>
         <form class= "form " action="" method="post" target="_blank">
 
-            <p>
+            Búscar producto: <input class="busqueda" type="search" name="busquedaproducto" placeholder="Search...">
 
-                Búscar producto: <input class="busqueda" type="search" name="busquedaproducto" placeholder="Search...">
-
-                <input type="submit" value="Buscar">
-                <input type="submit" value="agregar">
-                <input type="submit" value="eliminar">
-                <input type="submit" value="actualizar">
-
-            </p>
+            <input type="submit" value="Buscar">
+            <!-- <input type="submit" value="agregar">
+            <input type="submit" value="eliminar">
+            <input type="submit" value="actualizar"> -->
+            
+            
+            
 
         </form>
+        <button v-on:click="isVisible = !isVisible">Agregar</button>
+        <br>
         
+        <div v-show="isVisible">
+            <form v-on:submit.prevent="createProduct">
+                <!-- <input type="number" v-model="id" placeholder="id"> -->
+                <input type="text" v-model="producto.nombre" placeholder="Descripción">
+                <input type="number" v-model="producto.precio" placeholder="Precio">
+
+                <button type="submit">Confirmar</button>
+            </form>
+        </div>
+        <div >
+            <form v-on:submit.prevent="updateProduct">
+                <input type="number" v-model="producto.id" placeholder="id">
+                <input type="text" v-model="producto.nombre" placeholder="Descripción">
+                <input type="number" v-model="producto.precio" placeholder="Precio">
+
+                <button type="submit">Confirmar</button>
+            </form>
+        </div>
+        <div >
+            <form v-on:submit.prevent="deleteProduct">
+                <input type="number" v-model="producto.id" placeholder="id">
+
+                <button type="submit">Confirmar</button>
+            </form>
+        </div>
         <table class="table" border="4">
             <thead>
                 <tr>
                     <th width=30px>ID</th>
-                    <th>Nombre</th>
+                    <th>Descripción</th>
                     <th width=120px>Precio</th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="d in datos" v-bind:key="d">
+                <tr v-for="(d, i) in datos" v-bind:key="i">
                     <th>{{d.id}}</th>
                     <th>{{d.nombre}}</th>
                     <th>{{d.precio}} COP</th>
@@ -42,17 +68,27 @@
 import jwt_decode from "jwt-decode";
 import axios from 'axios';
 
+
 export default {
     name: "Productos",
     
     data: function(){
         return {
-            id: "",
-            nombre: "",
-            precio: 0,
-            loaded: false,
             
+            
+            producto:{
+                id: 0,
+                nombre: "",
+                precio: 0,
+                user:0,
+            },
+            loaded: false,
+            isVisible: false
         }
+    },
+
+    components: {
+        
     },
 
     methods: {
@@ -74,20 +110,131 @@ export default {
                     
                     this.loaded = true;
                     })
-                .catch(() => {
-                    this.$emit('logOut');
+                .catch((error) => {
+                    console.log("Error");
+                    if(error.response.status == "401") {
+                        alert("Usted no está autorizado para realizar esta operación.");
+                    }
+                    else if(error.response.status == "400"){
+                        alert("Revise todos los datos e intente de nuevo.");
+                    };
                 });
         },
 
-        verifyToken: function () {
-            return axios.post("https://mision-tic-bank-be00.herokuapp.com/refresh/", {refresh: localStorage.getItem("token_refresh")}, {headers: {}})
-				.then((result) => {
-					localStorage.setItem("token_access", result.data.access);		
-				})
-				.catch(() => {
-					this.$emit('logOut');
-				});
-        }
+        verifyToken: async function () {
+            try {
+            const result=await axios.post("https://mision-tic-bank-be00.herokuapp.com/refresh/", { refresh: localStorage.getItem("token_refresh") }, { headers: {} });
+            localStorage.setItem("token_access", result.data.access);
+          } catch(e) {
+            this.$emit('logOut');
+          }
+        },
+
+        createProduct: async function () {
+            if (localStorage.getItem("token_access") === null || localStorage.getItem("token_refresh") === null) {
+                this.$emit('logOut');
+                return;
+			}
+
+            await this.verifyToken();
+            
+            let token = localStorage.getItem("token_access");
+            let userId = jwt_decode(token).user_id;
+            
+            this.producto.user = userId;
+
+            axios.post(`https://mision-tic-bank-be00.herokuapp.com/producto/`,this.producto, 
+                                                                            {headers: {'Authorization': `Bearer ${token}`}})
+                .then((result) => {
+                    alert(result.data); 
+                    this.producto.nombre= "";
+                    this.producto.precio = 0;
+                    this.producto.user= 0;  
+                    console.log(this.producto);               
+                    
+                    })
+                .catch((error) => {
+                    console.log("Error");
+                    
+                    if(error.response.status == "401") {
+                        alert("Usted no está autorizado para realizar esta operación.");
+                    }
+                    else if(error.response.status == "400"){
+                        alert("Revise todos los datos e intente de nuevo.");
+                    }
+                });
+        },
+
+        updateProduct: async function () {
+            if (localStorage.getItem("token_access") === null || localStorage.getItem("token_refresh") === null) {
+                this.$emit('logOut');
+                return;
+			}
+
+            await this.verifyToken();
+            
+            let token = localStorage.getItem("token_access");
+            let userId = jwt_decode(token).user_id;
+            
+            this.producto.user = userId;
+
+            axios.put(`https://mision-tic-bank-be00.herokuapp.com/producto/update/${userId}/${this.producto.id}/`,this.producto, 
+                                                                            {headers: {'Authorization': `Bearer ${token}`}})
+                .then((result) => {
+                    alert("Item actualizado"); 
+                    this.producto.id = 0;
+                    this.producto.nombre= "";
+                    this.producto.precio = 0;
+                    this.producto.user= 0;  
+                    console.log(this.producto);               
+                    
+                    })
+                .catch((error) => {
+                    console.log("Error");
+                    
+                    if(error.response.status == "401") {
+                        alert("Usted no está autorizado para realizar esta operación.");
+                    }
+                    else if(error.response.status == "400"){
+                        alert("Revise todos los datos e intente de nuevo.");
+                    }
+                });
+        },
+
+        deleteProduct: async function () {
+            if (localStorage.getItem("token_access") === null || localStorage.getItem("token_refresh") === null) {
+                this.$emit('logOut');
+                return;
+			}
+
+            await this.verifyToken();
+            
+            let token = localStorage.getItem("token_access");
+            let userId = jwt_decode(token).user_id;
+            
+            this.producto.user = userId;
+
+            axios.delete(`https://mision-tic-bank-be00.herokuapp.com/producto/delete/${userId}/${this.producto.id}/`, 
+                                                                            {headers: {'Authorization': `Bearer ${token}`}})
+                .then((result) => {
+                    alert("Item eliminado"); 
+                    this.producto.id = 0;
+                     
+                    console.log(this.producto);               
+                    
+                    })
+                .catch((error) => {
+                    console.log("Error");
+                    
+                    if(error.response.status == "401") {
+                        alert("Usted no está autorizado para realizar esta operación.");
+                    }
+                    else if(error.response.status == "400"){
+                        alert("Revise todos los datos e intente de nuevo.");
+                    }
+                });
+        },
+
     },
 
     created: async function(){
